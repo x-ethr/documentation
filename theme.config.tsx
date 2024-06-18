66e90523e2bc760a1e701ee8de37576df0f8a960
Ethan <ethanzseligman@gmail.com>
2024-06-18 17:44:37 -0400

    Initial commit

---
+++ theme.config.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { DocsThemeConfig } from 'nextra-theme-docs'
+import Logo from "./components/logo";
+
+const config: DocsThemeConfig = {
+  logo: <Logo size={75} style={{color: "#FFFFFF"}}/>,
+  project: {
+    link: 'https://github.com/x-ethr',
+  },
+  chat: {
+    link: 'https://discord.gg/ethr',
+  },
+  docsRepositoryBase: 'https://github.com/x-ethr',
+  footer: {
+    text: 'X-ETHR, LLC',
+  },
+}
+
+export default config
